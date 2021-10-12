975e39b8fab9fb0d918e5e23fbad47ed5f9e9234
Celine Rolinda <celinerlnda@gmail.com>
2021-10-12 16:59:45 +0800

    Laravel Permission

---
+++ database/seeders/CreateAdminUserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Seeder;
+
+class CreateAdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+            'name' => 'Celine Rolinda', 
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('123456')
+        ]);
+    
+        $role = Role::create(['name' => 'Admin']);
+     
+        $permissions = Permission::pluck('id','id')->all();
+   
+        $role->syncPermissions($permissions);
+     
+        $user->assignRole([$role->id]);
+    }
+}
